8b92d39e871838b591cc67465f27b0f4798298b1
Ash <ash@ashryanbeats.com>
2017-10-03 10:56:04 -0400

    Fixed error checking syntax. Added some console output.

--- rascon.js
+++ rascon.js
@@ -2,18 +2,22 @@ const { exec } = require("child_process");
 
 // Get your IP address
 exec("ipconfig getifaddr en0", (err, stdout, stderr) => {
-  if (err) console.error(`ipconfig node error: ${err}`); return;
-  if (stderr) console.error(`ipconfig std error: ${stderr}`); return;
+  if (err) { console.error(`ipconfig node error: ${err}`); return; }
+  if (stderr) { console.error(`ipconfig std error: ${stderr}`); return; }
 
   const ip = stdout.trim();
   const nmapCmd = `sudo nmap -sn ${ip}/24`;
 
   // Use your IP address to scan all connected devices
   exec(nmapCmd, (err, stdout, stderr) => {
-    if (err) console.error(`nmap node error: ${err}`); return;
-    if (stderr) console.error(`nmap std error: ${stderr}`); return;
+
+    if (err) { console.error(`nmap node error: ${err}`); return; }
+    if (stderr) { console.error(`nmap std error: ${stderr}`); return; }
 
     const raspiIp = getRaspiIp(stdout);
+
+    console.log(`\nIP address found for Raspberry Pi: ${raspiIp}`);
+    console.log(`Copying the command to your clipboard`);
 
     // Copy the ssh command to the clipboard and exit
     exec(`echo "ssh pi@${raspiIp}" | pbcopy`);
