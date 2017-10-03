74d680c64674ba2593e3adbb5e9f3c912f3fd41e
Ash <ash@ashryanbeats.com>
2017-10-03 10:45:19 -0400

    Added comments. Broke IP matching out into helper function.

--- rascon.js
+++ rascon.js
@@ -1,5 +1,6 @@
 const { exec } = require("child_process");
 
+// Get your IP address
 exec("ipconfig getifaddr en0", (err, stdout, stderr) => {
   if (err) console.error(`ipconfig node error: ${err}`); return;
   if (stderr) console.error(`ipconfig std error: ${stderr}`); return;
@@ -7,13 +8,28 @@ exec("ipconfig getifaddr en0", (err, stdout, stderr) => {
   const ip = stdout.trim();
   const nmapCmd = `sudo nmap -sn ${ip}/24`;
 
+  // Use your IP address to scan all connected devices
   exec(nmapCmd, (err, stdout, stderr) => {
     if (err) console.error(`nmap node error: ${err}`); return;
     if (stderr) console.error(`nmap std error: ${stderr}`); return;
 
-    const res = stdout.match(/([\d.]+)\n.+\n.+Raspberry/);
-    if (!res || !res[1]) console.log("Raspberry Pi not found on network. Please try again."); return;
+    const raspiIp = getRaspiIp(stdout);
 
-    exec(`echo "ssh pi@${res[1]}" | pbcopy`);
+    // Copy the ssh command to the clipboard and exit
+    exec(`echo "ssh pi@${raspiIp}" | pbcopy`);
   });
 });
+
+function getRaspiIp(stdout) {
+  // Match for an IP address followed by "Raspberry" 2 lines below
+  const match = stdout.match(/([\d.]+)\n.+\n.+Raspberry/);
+
+  // Exit if not found
+  if (!match || !match[1]) {
+    console.log("Raspberry Pi not found on network. Please try again.");
+    process.exit();
+  }
+
+  // Return the regex capture group containing the raspi IP address
+  return match[1];
+}
